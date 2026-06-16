82aad8f08ffffdc8a99a5691dff5ade899def137
saamyaksingh <saamyaksingh.dev@gmail.com>
2026-06-16 19:09:04 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,22 @@
+a=1234
+print(a)
+a=1255
+f=a
+print (f)
+a= "Saamyak Singh"
+print(a.upper())
+print(a.lower())
+print(a.isdigit())
+print(a.islower())
+print(a.isupper())
+print(a.isspace())
+print(a[::-1])
+print(a.strip())
+print(a.isalpha())
+print(a.isalnum())
+a=input("Enter your name:")
+print(a)
+b=float(input("enter your password"))
+print(b)
+r=int(input("Enter Your Roll no:"))
+print(r)
