d6a7225a27e2a88f2dcf96e9cc834119dfa2f186
Saamyak Singh <saamyaksingh.dev@gmail.com>
2026-06-17 21:18:56 +0530

    Add files via upload
    day 2

---
+++ Arthmetic Progression.py
@@ -0,0 +1,50 @@
+ 
+print("Calculate the sum of 3 numbers")
+a=int(input("First No:"))
+b=int(input("Second No:"))
+c=int(input("Third No:"))
+print(a+b+c)
+
+print("Calculate the area of square")
+a=int(input("Length of side:"))
+print(a*a)
+
+print("calculate the parameter of square")
+a=int(input("Length of Side"))
+print(4*a)
+
+print("calculate the area of rectangle")
+a=int(input("Length"))
+b=int(input("Width"))
+print(a*b)
+
+print("calculate the perimeter of rectangle")
+a=int(input("Length"))
+b=int(input("Width"))
+print(2*(a+b))
+
+print("calculate the parimeter of triangle")
+a=int(input("1st side:"))
+b=int(input("2nd side:"))
+c=int(input("3rd side:"))
+print(a+b+c)
+
+
+print("Calculate the area of circle")
+a=float(input("Radius"))
+print(3.14*(a*a))
+
+
+print("calculate the simple intrest")
+a=int(input("Principal:"))
+b=int(input("Rate:"))
+c=int(input("Time:"))
+print((a*b*c)/100)
+
+print("enter 5 number calculate the total and average")
+a=int(input("1st no"))
+b=int(input("2nd no"))
+c=int(input("3rd no"))
+d=int(input("4th no"))
+e=int(input("5th no"))
+print((a+b+c+d+e)/5)
